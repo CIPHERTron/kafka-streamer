07ff6531a7029abd9599032f5f8a4193ca7fe94c
CIPHERTron <pritish.samal918@gmail.com>
2023-06-10 14:17:13 +0530

    feat: add get all orders controller

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from flask import Flask, request
+from os import environ
+import psycopg2
 
 app = Flask(__name__)
 
+def create_connection():
+    conn = psycopg2.connect(
+        host='localhost',
+        database='orders',
+        user=environ.get("POSTGRES_USERNAME"),
+        password=environ.get("POSTGRES_PASSWORD"),
+    )
+    return conn
 
 @app.route("/")
 def hello():
@@ -11,9 +21,11 @@ def hello():
 @app.route("/orders", methods=["POST", "GET"])
 def orders():
     if request.method == "GET":
-        return "GET route hit"
-    else:
-        return "POST route hit."
+        conn = create_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM orders;")
+        orders = cursor.fetchall()
+        return orders
 
 
 if __name__ == "__main__":
