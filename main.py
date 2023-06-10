f97447e274802042b2ce9abc8e58b7ae9f35f6f7
CIPHERTron <pritish.samal918@gmail.com>
2023-06-10 07:35:06 +0530

    feat: setup basic flask

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hey there, welcome to the Kafka streamer!"
+
+
+@app.route("/orders", methods=["POST", "GET"])
+def orders():
+    if request.method == "GET":
+        return "GET route hit"
+    else:
+        return "POST route hit."
+
+
+if __name__ == "__main__":
+    app.run()
